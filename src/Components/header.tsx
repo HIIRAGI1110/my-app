c151ebc65bdf457c13d95e611cbac672b962ef79
HIIRAGI1110 <buryuunaku@gmail.com>
2023-12-08 09:51:41 +0900

    あ

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
           <div><a href="/Achieve">制作実績</a></div>
           <div><a href="/">依頼詳細</a></div>
           <div><a href="/Myself">その他</a></div>
-        </div>
+        </div> 
         <div className='headerIcons'>
           <div><a href="https://x.com/HIIRAGI_MV?s=20"><FaSquareXTwitter size={40} /></a></div>
           <div><a href="https://youtube.com/playlist?list=PL9RF1Bly0GN117-1DIbtW5RD7T08fHBxi&si=wKY80Q64CtBkFLh8"><FaYoutube size={40} /></a></div>
